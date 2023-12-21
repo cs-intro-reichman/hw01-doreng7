941c3bc3dde28c6c782ef9d0b22a5af341c26fa1
DorEng7 <dor.englender@post.runi.ac.il>
2023-12-22 00:29:13 +0200

    impliment programs (Lin , Tri)

--- Triangle.java
+++ Triangle.java
@@ -1,10 +1,14 @@
-/*
- * Three sides can form a triangle if the sum of the lengths of any two sides is greater than the length of the remaining side. 
- * This is known as the Triangle Inequality Theorem. 
- * Write a program that tests if three given integers form a triangle.
- */ 
-public class Triangle {
-	public static void main(String[] args) {
-		// Put your code here	
+
+public class Triangle
+	{
+		public static void main(String[] args)
+		{
+			//program that gets 3 lenghs and prints "true" if these 3 lenghs can form a triangle, otherwise prints "false"
+			int Side1 = Integer.parseInt(args[0]);
+			int side2 = Integer.parseInt(args[1]);
+			int Side3 = Integer.parseInt(args[2]);
+			boolean Triangle = (Side1 + Side2) > Side3 && (Side1 +Side3) > Side2 && (Side2 + Side3) > Side1;
+			System.out.println(Side1 + "," + Side2 + "," + Side3 + ":" + Triangle);
+			
+		}
 	}
-}
