5ecc405d1b112dc68dbb4239121d026b08a83f2c
doreng7 <154465261+doreng7@users.noreply.github.com>
2023-12-22 17:27:48 +0200

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -5,7 +5,7 @@ public class Triangle
 		{
 			//program that gets 3 lenghs and prints "true" if these 3 lenghs can form a triangle, otherwise prints "false"
 			int Side1 = Integer.parseInt(args[0]);
-			int side2 = Integer.parseInt(args[1]);
+			int Side2 = Integer.parseInt(args[1]);
 			int Side3 = Integer.parseInt(args[2]);
 			boolean Triangle = (Side1 + Side2) > Side3 && (Side1 +Side3) > Side2 && (Side2 + Side3) > Side1;
 			System.out.println(Side1 + "," + Side2 + "," + Side3 + ":" + Triangle);
